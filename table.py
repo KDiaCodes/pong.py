682ada02e30d6434831a64ff7e54d5d420379a43
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-04-20 18:36:07 +0200

    Update table.py

--- table.py
+++ table.py
@@ -6,3 +6,12 @@ class Table:
     self.width = width
     self.height = height
     self.colour = colour
+    
+    self.canvas = Canvas(window, bg=self.colour, height=self.height, width=self.width)
+    self.canvas.pack()
+    
+    if(vertical_net) :
+      self.canvas.create_line(self.width/2, 0, self.width/2, self.height, width=2, fill=net_colour, dash= (15, 23))
+      
+    if(horizontal_net) :
+      self.canvas.create_line(0, self.height/2, self.width, self.height/2, width=2, fill=net_colour, dash= (15, 23))
