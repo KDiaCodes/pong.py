366f40fe9a875d1581b452a595c2e86919c77d6a
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-04-20 18:30:48 +0200

    Update table.py

--- table.py
+++ table.py
@@ -1,3 +1,8 @@
 from tKinter import *
 
-classTable:
+class Table:
+  ###constructor
+  def __init__(self, window, colour="black", net_colour="green", width=600, height=400, vertical_net=False, horizontal_net=False):
+    self.width = width
+    self.height = height
+    self.colour = colour
